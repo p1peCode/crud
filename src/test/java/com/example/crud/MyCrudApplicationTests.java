c28ee3562999a3fae6ceffbb0f4921006f859e01
Valery <valeryroj@yandex.ru>
2024-06-08 18:39:17 +0300

    added env var

--- src/test/java/com/example/crud/MyCrudApplicationTests.java
+++ src/test/java/com/example/crud/MyCrudApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.crud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MyCrudApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
